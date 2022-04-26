e49c4dcc9d4bab6d5d73d477bca70347388dea49
Adetunji Ojekunle <ojekunleadetunji@gmail.com>
2022-04-26 18:43:27 +0100

    fixed error: replaced <a> for <Link>

--- components/top-header.tsx
+++ components/top-header.tsx
@@ -125,17 +125,20 @@ export default function TopHeader() {
 
                                                 <Menu.Item>
                                                     {({ active }) => (
-                                                        <a
-                                                            href="/"
-                                                            className={classNames(
-                                                                active
-                                                                    ? "bg-gray-100"
-                                                                    : "",
-                                                                "block px-4 py-2 text-sm text-gray-700"
-                                                            )}
-                                                        >
-                                                            sich abmelden
-                                                        </a>
+                                                        <Link href="/">
+
+                                                            <a
+
+                                                                className={classNames(
+                                                                    active
+                                                                        ? "bg-gray-100"
+                                                                        : "",
+                                                                    "block px-4 py-2 text-sm text-gray-700"
+                                                                )}
+                                                            >
+                                                                sich abmelden
+                                                            </a>
+                                                        </Link>
                                                     )}
                                                 </Menu.Item>
                                             </Menu.Items>
