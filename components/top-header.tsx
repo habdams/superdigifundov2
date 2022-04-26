fc85184ff65d432198ba86314e2b01a4301e8e29
Adetunji Ojekunle <ojekunleadetunji@gmail.com>
2022-04-26 22:54:27 +0100

    fixed top header image error

--- components/top-header.tsx
+++ components/top-header.tsx
@@ -23,12 +23,12 @@ export default function TopHeader() {
                             <div className="flex justify-between h-16">
                                 <div className="flex">
                                     <div className="flex-shrink-0 flex items-center">
-                                        <Image
+                                        <img
                                             className="block lg:hidden h-8 w-auto"
                                             src="./digifundo.png"
                                             alt="DigiFundo Logo"
                                         />
-                                        <Image
+                                        <img
                                             className="hidden lg:block h-8 w-auto"
                                             src="./digifundo.png"
                                             alt="DigiFundo Logo"
@@ -202,7 +202,7 @@ export default function TopHeader() {
                             <div className="pt-4 pb-3 border-t border-gray-200">
                                 <div className="flex items-center px-4">
                                     <div className="flex-shrink-0">
-                                        <Image
+                                        <img
                                             className="h-10 w-10 rounded-full"
                                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                                             alt="image"
